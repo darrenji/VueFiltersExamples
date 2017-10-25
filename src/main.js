1e024505838f8bcaaeaffd6dae614714e710277a
darren <764190362@qq.com>
2017-10-25 10:29:44 +0800

    全局mixin

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ Vue.filter('to-lowercase', function(value){
     return value.toLowerCase();
 });
 
+//全局mixin
+Vue.mixin({
+    created(){
+        console.log('Global Mixin - Created Hook');
+    }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
